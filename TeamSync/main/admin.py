a813d2a029b05b71a04fe2386923a6defc763834
shah <apekshashah029@gmail.com>
2024-03-06 17:18:27 +0530

    models added

--- TeamSync/main/admin.py
+++ TeamSync/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ProjectList,Project_History,Chat
 # Register your models here.
+admin.site.register(ProjectList)
+admin.site.register(Project_History)
+admin.site.register(Chat)
--- TeamSync/main/models.py
+++ TeamSync/main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProjectList(models.Model):
+   fkey = models.ForeignKey(User, on_delete=models.CASCADE)
+   name = models.CharField(max_length = 100)
+   status =  models.BooleanField(default = False)
+   name1 = models.CharField(max_length = 100 , null = True)
+
+   @property   
+   def CreaterID(self):
+       return self.fkey.userId
+  
+   def ProjectContributors(self):
+       return self.fkey.userId
+  
+   def __str__(self):
+       return self.name1
+
+
+class Project_History(models.Model):
+   contributorId = models.ForeignKey(User, on_delete=models.CASCADE)
+   project_name = models.CharField(max_length=255)
+   description = models.TextField()
+
+
+   def __str__(self):
+       return self.project_name
+
+
+class Chat(models.Model):
+   fkey = models.ForeignKey(User, on_delete=models.CASCADE)
+   chatId = models.CharField(max_length = 10)
+   message = models.CharField(max_length = 1000, null = True)
+
+
+   @property
+   def senderId(self):
+       return self.fkey.userId
+   def recieverID(self):
+       return self.fkey.userId
